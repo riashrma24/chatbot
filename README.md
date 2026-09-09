c5a11e92b67c9d9d44f5deec72502ccb6dfa324c
Riya Sharma <riya.sharma@opensenselabs.com>
2026-09-09 15:14:52 +0530

    Phase 2 : Adding manually managed memory

---
+++ README.md
@@ -0,0 +1,27 @@
+# Phase 1
+
+Your Python program
+       ↓
+OpenAI API
+       ↓
+GPT model
+       ↓
+response
+       ↓
+Your Python program
+
+# Phase 2 - giving it memory
+You are sending the previous conversation to the model every time.
+
+messages
+    ↓
+user message
+    ↓
+LLM
+    ↓
+assistant message
+    ↓
+messages
+
+
+
--- backend/chatbot.py
+++ backend/chatbot.py
@@ -1,25 +1,49 @@
 import os
-
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 
-client=genai.Client(
-    api_key=os.getenv('GENAI_API_KEY')
-)
+client = genai.Client(api_key=os.getenv('GENAI_API_KEY'))
+
+messages = []
 
 def ask_llm(message):
+    messages.append(
+        {
+            "type":"user_input",
+            "content":[
+                {
+                    "type": "text",
+                    "text":user_message
+                }
+            ]
+        }
+    )
+
     response=client.interactions.create(
-        model="gemini-3.5-flash-lite",
-        input=message
+        model='gemini-3.5-flash-lite',
+        input=messages
+    )
+
+    messages.append(
+        {
+            "type":"model_output",
+            "content":[
+                {
+                    "type": "text",
+                    "text":response.output_text
+                }
+            ]
+        }
     )
 
     return response.output_text
 
+
 while True:
-    user_message=input("You : ")
+    user_message=input("you : ")
     if user_message.lower() == 'exit':
         break
-    answer=ask_llm(user_message)
-    print("Bot : ",answer)
+    ans=ask_llm(user_message)
+    print("bot : ", ans)
